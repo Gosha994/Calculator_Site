42d8a373dc87de7f89e9a58bf97e20319c7faa62
MrSteam <156875858+gosha994@users.noreply.github.com>
2026-04-24 21:55:35 +0300

    Исправления

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
+from flask import Flask, render_template
 
 import datetime
-
 
 app = Flask(__name__)
 
@@ -11,6 +11,8 @@ def index():
     print("Успешное подключение:", datetime.datetime.now())
     return render_template("index.html", title="Главная страница")
 
+
+port = 5000
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=port)
